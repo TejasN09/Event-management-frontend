4d7e9f51cbe10657043d972796f182a63071775f
Tejas N <maheshteju2002@gmail.com>
2023-02-24 20:57:36 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import "./App.css";
-
+import React, { useState } from 'react';
+import './App.css';
+import Header from './components/header/Header';
+import Footer from './components/footer/Footer';
 import Loginform from "./components/Loginform";
+
+
 function App() {
   return (
     <div className="body">
+       <Header />
+        <h1>Event Management</h1>
+       <Footer />
       <Loginform></Loginform>
     </div>
   );
