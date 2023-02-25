61c8d8b9c0fe7cba40a26911cadbe640b4bd9e87
Tejas N <maheshteju2002@gmail.com>
2023-02-25 15:18:17 +0530

    added assets img

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
 import "./App.css";
 import SignupForm from "./components/SignupForm";
+import LoginForm from "./components/Loginform";
+
+
 function App() {
   return (
     <div className="body">
       <h1>Event Management</h1>
-      <SignupForm></SignupForm>
+      {/* <SignupForm></SignupForm> */}
+      <LoginForm />
+
     </div>
   );
 }
