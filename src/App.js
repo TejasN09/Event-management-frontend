43566c12338f4bc94141313cc84087aaebe8b015
Tejas N <maheshteju2002@gmail.com>
2023-02-25 15:23:39 +0530

    added home

--- src/App.js
+++ src/App.js
@@ -2,15 +2,19 @@ import React from "react";
 import "./App.css";
 import SignupForm from "./components/SignupForm";
 import LoginForm from "./components/Loginform";
-
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+import Home from "./components/home/Home";
 
 function App() {
   return (
     <div className="body">
-      <h1>Event Management</h1>
+      {/* <h1>Event Management</h1> */}
+      <Header />
       {/* <SignupForm></SignupForm> */}
-      <LoginForm />
-
+      {/* <LoginForm /> */}
+      <Home />
+      <Footer />
     </div>
   );
 }
