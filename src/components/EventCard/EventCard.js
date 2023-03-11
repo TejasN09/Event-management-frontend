d710ade61cff37389698b8cd9b60059e99038615
khanshujaath007 <khanshujaath007@gmail.com>
2023-03-12 00:36:53 +0530

    home page design

--- src/components/EventCard/EventCard.js
+++ src/components/EventCard/EventCard.js
@@ -1,11 +1,17 @@
+<<<<<<< HEAD:src/components/EventCard/EventCard.js
 
 import React from 'react';
 import Header from '../header/Header';
 import "./EventCard.css"
+=======
+import React from "react";
+import Header from "./header/Header";
+>>>>>>> 5b96d9b (Home page design):src/components/EventCard.js
 
 const EventCard = ({ title, description }) => {
   return (
     <>
+<<<<<<< HEAD:src/components/EventCard/EventCard.js
     {/* <Header/> */}
       <div className="event-card-container max-w-sm bg-white border border-gray-200 rounded-xl drop-shadow-2xl dark:bg-gray-800 dark:border-gray-700 mt-10">
         <a href="#">
@@ -15,34 +21,45 @@ const EventCard = ({ title, description }) => {
             alt=""
           />
         </a>
+=======
+      {/* <Header/> */}
+      <div className="bg-gray-400 dark:bg-slate-100 mb-auto max-w-sm m-1 border-fuchsia-600 border-1 p-1 rounded-xl drop-shadow-2xl ">
+        <img
+          className="rounded-xl p-1 border-1 drop-shadow-2xl"
+          src="https://images.pexels.com/photos/976866/pexels-photo-976866.jpeg?auto=compress&cs=tinysrgb&w=600"
+          alt=""
+        />
+>>>>>>> 5b96d9b (Home page design):src/components/EventCard.js
         <div className="p-2">
-          <a href="#">
-            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">
+          <a href="#" className="no-underline ">
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-stone-900">
               {title}
             </h5>
           </a>
           <p className="mb-2 font-normal text-gray-700 dark:text-gray-400">
             {description}
           </p>
-          <a
-            href="#"
-            className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-          >
-            Read more
-            <svg
-              aria-hidden="true"
-              className="w-4 h-4 ml-2 -mr-1"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
+          <div>
+            <a
+              href="#"
+              className="no-underline inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-voilet-600 rounded-lg hover:bg-voilet-800 focus:ring-4 focus:outline-none focus:ring-fuchsia-200 dark:bg-fuchsia-800 dark:hover:bg-fuchsia-600 dark:focus:ring-fuchsia-800"
             >
-              <path
-                fillRule="evenodd"
-                d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                clipRule="evenodd"
-              ></path>
-            </svg>
-          </a>
+              Read more
+              <svg
+                aria-hidden="true"
+                className="w-4 h-4 ml-2 -mr-1"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                  clipRule="evenodd"
+                ></path>
+              </svg>
+            </a>
+          </div>
         </div>
       </div>
     </>
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,35 +1,3 @@
-// import React from "react";
-// import homeImg from "./../../assets/HomePage_main.jpg";
-// import Header from "../header/Header";
-// import "./Home.css";
-// import EventCard from "../EventCard";
-// const Home = () => {
-//   // return (
-//   //   <section className="text-gray-600 body-font">
-//   //     <Header />
-//   //     <div className="home w-full mx-auto">
-//   //       <div className="home__container ">
-//   //         <img src={homeImg} alt="homePage" className="relative w-fit left-2/4 h-72 object-cover  " />
-//   //         <div className="home__content items-center justify-center flex mt-1 absolute ">
-//   //           <h1 className="home__content__title relative mb-1 text-black font-bold text-lg text-center ">
-//   //             Event Management
-//   //           </h1>
-//   //           <h2 className="home__content__des absolute text-black text-lg">Event management is the application of project management to the creation and development of small and/or
-//   //             large-scale personal or corporate events such as festivals, conferences, ceremonies, weddings, formal parties,
-//   //             concerts, or conventions. It involves studying the brand, identifying its target audience, devising the event
-//   //             concept, and coordinating the technical aspects before actually launching the event</h2>
-//   //           <h2 className="try__it text-black text-bold text-xl absolute ml-0">Try it!</h2>
-//   //           <button className="home__content__btn absolute bg-blue-500 rounded-md ml-36 px-2 py-2 text-center text-white hover:bg-slate-500">
-//   //             Get Started
-//   //           </button>
-//   //         </div>
-//   //       </div>
-//   //     </div>
-//   //   </section>
-//   // )
-// }
-
-// export default Home;
 import React from "react";
 import homeImg from "./../../assets/HomePage_main.jpg";
 import "./Home.css";
@@ -41,26 +9,22 @@ const Home = () => {
   return (
     <>
       <Header />
-      <div className="flex flex-col md:flex-row items-center justify-center md:justify-between absolute">
+      <div className="flex flex-col sm:flex-row items-center justify-center sm:justify-between absolute">
         <div className="md:w-1/2">
           <img src={homeImg} alt="homePage" className="w-full" />
         </div>
         <div className="md:w-1/2 text-center md:text-left ">
-          <div className="writer">
-            <div className="writer-text">
-              Host Events
-            </div>
+          <div className="writer ">
+            <div className="writer-text font-sans">Host Events</div>
           </div>
           <p className="text-xl mt-4">Hello world</p>
-          <button className="mt-8 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">Get Started</button>
+          <button className="mt-8 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">Learn More</button>
         </div>
+        {/* <EventCard title="Chess" description={"Chess tournament"} />
+      <EventCard title="Dancing stars" description={"Dance Competition"} /> */}
       </div>
-      <div className="mt-[30rem] cardListing">
-        <h1 className="flex items-center justify-center h-10">Some Ongoing Events</h1>
-        <EventCard title={"Tejas N"} description={"Im a man"} />
-        <EventCard title={"Shujaath khan"} description={"he is women"} />
-        <EventCard title={"Chess Game"} description={"Chess Game is wonderful game"} />
-        <EventCard title={"Tic tac tao"} description={"Tic tac tao is wonderful game"} />
+      <div className="mt-[30rem]"> 
+        <EventCard title="Tejas N" description={"Im a man"} />
       </div>
       <Footer />
     </>
